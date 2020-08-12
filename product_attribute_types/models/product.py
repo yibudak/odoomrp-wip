c8db6a807104059a168f6e5fab455c370abf2c15
Doğan ALTUNBAY <dogan.altunbay@codequarters.com>
2020-08-12 14:45:35 +0300

    view fixing

--- product_attribute_types/models/product.py
+++ product_attribute_types/models/product.py
@@ -4,7 +4,7 @@
 # Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields
+from odoo import api, fields, models
 
 
 class ProductAttribute(models.Model):
@@ -23,7 +23,7 @@ class ProductAttribute(models.Model):
 
 
 class ProductAttributeLine(models.Model):
-    _inherit = "product.attribute.line"
+    _inherit = "product.template.attribute.line"
 
     required = fields.Boolean('Required')
     default = fields.Many2one('product.attribute.value', 'Default')
@@ -39,3 +39,43 @@ class ProductAttributeValue(models.Model):
     numeric_value = fields.Float('Numeric Value', digits=(12, 6))
     min_range = fields.Float('Min', digits=(12, 6))
     max_range = fields.Float('Max', digits=(12, 6))
+
+    @api.onchange('name')
+    def onchange_name(self):
+        if self.attr_type == 'numeric':
+            try:
+                self.numeric_value = float((''.join([c for c in self.name if c in '1234567890,.'])).replace(',', '.'))
+                self.attribute_code = (''.join([c for c in self.name if c in '1234567890,.'])).replace(',', '').replace('.', '')
+            except Exception:
+                pass
+
+    @api.one
+    def write(self, vals):
+        if vals.get('name',False):
+            if vals.get('attr_type') == 'numeric' or self.attr_type == 'numeric':
+                if vals.get('numeric_value',0.0) == 0.0 or self.numeric_value == 0.00:
+                    try:
+                        vals['numeric_value'] = float((''.join([c for c in vals.get('name','') if c in '1234567890,.'])).replace(',', '.'))
+                    except Exception:
+                        pass
+        return super(ProductAttributeValue, self).write(vals)
+
+    @api.model
+    def create(self, vals):
+        create_vals = super(ProductAttributeValue, self).create(vals)
+        if vals.get('name',False):
+            if create_vals['attr_type'] == 'numeric':
+                if create_vals['numeric_value'] == 0.0:
+                    try:
+                        create_vals['numeric_value'] = float((''.join([c for c in vals.get('name','') if c in '1234567890,.'])).replace(',', '.'))
+                    except Exception:
+                        pass
+
+                if create_vals['numeric_value'] != 0.0:
+                    try:
+                        create_vals['attribute_code'] = (''.join([c for c in vals.get('name','') if c in '1234567890,.'])).\
+                            replace(',','').replace('.', '')
+                    except Exception:
+                        pass
+
+        return create_vals
